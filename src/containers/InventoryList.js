a89d18a9889f07421accababd2819e9eb1011703
Audra <audra.tallman@hotmail.com>
2019-12-08 13:43:48 -0600

    initial commit, modify app.js, add containers and components.

---
+++ src/containers/InventoryList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class InventoryList extends Component {
+  render() {
+    return(
+      <div className="list">
+      These are the Items currently in your kitchen:
+
+      </div>
+    )
+  }
+}
+
+export default InventoryList;
